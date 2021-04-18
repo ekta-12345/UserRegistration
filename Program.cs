2a10a7fce9fa43e6b651ab0b6e5a60c1f2926957
ekta12345 <ektakumbhare3@gmail.com>
2021-04-18 22:53:21 +0530

    Validting a last name

--- Program.cs
+++ Program.cs
@@ -5,20 +5,28 @@ namespace UserRegistration
 {
     class Program
     {
-        static int num = 0;
+        static int Fname = 0;
+        static int Lname = 0;
         static void Main(string[] args)
         {
 
             string pattern = "^[A-Z]{1,}[A-Za-z]{2,}";
-            string[] name = new string[num + 1];
+            string[] first = new string[Fname + 1];
+            string[] last = new string[Lname + 1];
+
 
             Console.WriteLine("Welcome to USER REgistration");
 
-            Console.WriteLine("Enter your Name: ");
-            name[num] = Console.ReadLine();
-            ValidName(name, pattern);
+            Console.WriteLine("Enter your First Name: ");
+            first[Fname] = Console.ReadLine();
+            FirstName(first, pattern);
+
+            Console.WriteLine("Enter your Last Name: ");
+            last[Lname] = Console.ReadLine();
+            LastName(last, pattern);
+
         }
-        public static void ValidName(string[] name, string pattern)
+        public static void FirstName(string[] name, string pattern)
         {
             Console.WriteLine("Validating a Name: ");
             Regex regex = new Regex(pattern);
@@ -27,14 +35,34 @@ namespace UserRegistration
                 bool result = regex.IsMatch(name[i]);
                 if (result)
                 {
-                    Console.WriteLine("Valid Name---->" + name[i]);
+                    Console.WriteLine("Valid FIrst Name---->" + name[i]);
                 }
                 else
                 {
-                    Console.WriteLine("InValid Name---->" + name[i]);
+                    Console.WriteLine("InValid First Name---->" + name[i]);
                 }
             }
 
         }
+        public static void LastName(string[] name, string pattern)
+        {
+            Console.WriteLine("Validating a Name: ");
+            Regex regex = new Regex(pattern);
+            for (int i = 0; i < name.Length; i++)
+            {
+                bool result = regex.IsMatch(name[i]);
+                if (result)
+                {
+                    Console.WriteLine("Valid Last Name---->" + name[i]);
+                }
+                else
+                {
+                    Console.WriteLine("InValid Last Name---->" + name[i]);
+                }
+            }
+
+        }
+
     }
 }
+
