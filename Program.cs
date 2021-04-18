f346ca3d47c3eb34774eab1869355756db8c9264
ekta12345 <ektakumbhare3@gmail.com>
2021-04-18 22:38:27 +0530

    Displaying message for User Registration

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static int num = 0;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to User Registration");    
+
+        }
+    }
+}
